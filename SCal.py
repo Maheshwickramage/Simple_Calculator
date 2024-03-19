dfd1432d5fdd1b5836546d940b4d171341bb9054
Maheshwickramage <139673873+Maheshwickramage@users.noreply.github.com>
2024-03-19 09:16:11 +0530

    Update_cal

--- SCal.py
+++ SCal.py
@@ -6,7 +6,8 @@ def subtraction(a, b):
 
 def division(a, b):
     if b == 0:
-        raise ValueError("Division by zero is not allowed")
+        print("float division by zero")
+        return None
     else:
         return a / b
 
@@ -23,23 +24,30 @@ def remainder(a, b):
         return a % b
 
 def reset():
-    print("Reset")
+    print("0$")
     return None
 
 def select_op(choice, a, b):
     if choice == "#":
+        
         return -1
     elif choice == "+":
+        
         return addition(a, b)
     elif choice == "-":
+       
         return subtraction(a, b)
     elif choice == "*":
+        
         return multiplication(a, b)
     elif choice == "/":
+       
         return division(a, b)
     elif choice == "^":
+       
         return power(a, b)
     elif choice == "%":
+       
         return remainder(a, b)
     else:
         print("Unrecognized operation")
@@ -51,7 +59,8 @@ def get_number_input(prompt):
             num = float(input(prompt))
             return num
         except ValueError:
-            print("Not a valid number, please enter again")
+         
+            return None
         except EOFError:
             print("Input interrupted, please try again.")
             return None
@@ -59,32 +68,42 @@ def get_number_input(prompt):
 # Main loop
 while True:
     print("Select operation.")
-    print("1. Add       : + ")
-    print("2. Subtract  : - ")
-    print("3. Multiply  : * ")
-    print("4. Divide    : / ")
-    print("5. Power     : ^ ")
-    print("6. Remainder : % ")
-    print("7. Terminate : # ")
-    print("8. Reset     : $ ")
+    print("1.Add      : + ")
+    print("2.Subtract : - ")
+    print("3.Multiply : * ")
+    print("4.Divide   : / ")
+    print("5.Power    : ^ ")
+    print("6.Remainder: % ")
+    print("7.Terminate: # ")
+    print("8.Reset    : $ ")
 
     # take input from the user
-    choice = input("Enter choice (+,-,*,/,^,%,#,$): ")
+    choice = input("Enter choice(+,-,*,/,^,%,#,$): ")
+    print(choice)
     
     if choice == "#":
+        
         print("Done. Terminating")
         break
     elif choice == "$":
         reset()
+  
     else:
         try:
             a = get_number_input("Enter first number: ")
+            
             if a is None:
+                print("0$")
                 continue
+            print(int(a))
             b = get_number_input("Enter second number: ")
+            
             if b is None:
+                print("0$")
                 continue
+            print(int(b))
+           
             result = select_op(choice, a, b)
-            print("Result:", result)
+            print(a,choice,b,"=",result)
         except ValueError as e:
             print("Error:", e)
